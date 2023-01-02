7d056721c2c361f03ea09f7c45c27b2d9c54cb28
veda-1716 <vedadhanush8@gmail.com>
2023-01-02 01:40:31 +0000

    prodcuts route error solved....

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -16,7 +16,7 @@
                 </div>
             </div>
             <div class="product-content">
-                <h4><a href="{{ route('single_product', $product->id) }}">{{ $product->name }}</a></h4>
+                <h4><a href="{{ route('shop', $product->id) }}">{{ $product->name }}</a></h4>
                 <p class="price">${{ $product->price }}</p>
             </div>
         </div>
