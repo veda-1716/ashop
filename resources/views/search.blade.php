a25ee338424b5939b44283ce9f18b53147a75ed4
veda-1716 <vedadhanush8@gmail.com>
2023-01-02 07:16:27 +0000

    new products and search blade added and changed...

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -30,7 +30,7 @@
                                 </div>
                             </div>
                             <div class="product-content">
-                                <h4><a href="{{ route('single_product', $product->id) }}">{{ $product->name }}</a></h4>
+                                <h4><a href="{{ route('show_single_product', $product->id) }}">{{ $product->name }}</a></h4>
                                 <p class="price">${{ $product->price }}</p>
                             </div>
                         </div>
